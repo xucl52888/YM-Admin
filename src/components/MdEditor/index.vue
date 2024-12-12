ca3e66546ede7903fb8039ec91724f797da06d7b
xcl <570636231@qq.com>
2024-12-12 23:02:39 +0800

    feat：增加富文本功能

---
+++ src/components/MdEditor/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <MdEditor v-model="props.modelValue" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { MdEditor } from 'md-editor-v3'
+import 'md-editor-v3/lib/style.css'
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+  config: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+const text = ref('')
+</script>
---
+++ src/views/componentExample/editor/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-card style="border: 0" shadow="never">
+    <MdEditor v-model="text" :theme="useProjectSetting.elementTheme" :toolbars="toolbars" style="height: 80vh">
+      <!-- <template #defToolbars>
+        <Emoji />
+      </template> -->
+    </MdEditor>
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { MdEditor } from 'md-editor-v3'
+import 'md-editor-v3/lib/style.css'
+import { useProjectSettingStore } from '@/store/projectSetting'
+const useProjectSetting = useProjectSettingStore()
+import { toolbars } from './toolbars'
+// import { Emoji } from '@vavt/v3-extension'
+
+const text = ref('# Hello Editor')
+</script>
+
+<style scoped lang="scss"></style>
