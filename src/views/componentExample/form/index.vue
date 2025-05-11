65a0f3b24fb25cca9ea406e0b736d33dc880fecd
xcl <570636231@qq.com>
2025-05-11 21:22:26 +0800

    feat：修复已知问题

--- src/views/componentExample/form/index.vue
+++ src/views/componentExample/form/index.vue
@@ -179,7 +179,7 @@ const submitForm = async (formEl: FormInstance | undefined) => {
   if (!formEl) return
   await formEl.validate((valid, fields) => {
     if (valid) {
-      console.log('submit!')
+      console.log('submit!', ruleForm)
     } else {
       console.log('error submit!', fields)
     }
